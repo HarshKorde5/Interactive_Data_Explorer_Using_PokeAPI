57a5ffc204abb31c9eb75cbdb996f162abaa1d57
HarshKorde5 <harshkorde05@gmail.com>
2025-04-28 20:09:35 +0530

    project creation and tailwindcss setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='bg-gray-300 p-4 rounded-xl m-4 text-center text-xl'>Interactive Data Explorer using PokeAPI!</h1>
+    </>
+  )
+}
+
+export default App
